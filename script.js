8537be66725d7847b5d71bef1719fd86b49fd3eb
camilapoblete <90003734+camilapoblete@users.noreply.github.com>
2021-09-08 18:55:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -27,8 +27,8 @@ var lorenIpsum = ["Irlanda","Noruega","Nueva Zelanda","Canadá","Australia"];
 function setup() {
     noCanvas();
     createElement("h1", elSaludo).parent(elEncabezado).id("title");
-    createA("index.html", "index").parent("vinculos");
-    createA("page.html", "page").parent("vinculos");
+    createA("index.html", "Portada").parent("vinculos");
+    createA("page.html", "Página").parent("vinculos");
     if (dondeEstas) {
         portada();
     } else {
